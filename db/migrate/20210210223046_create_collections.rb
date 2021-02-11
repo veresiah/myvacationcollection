0ab11344011707be7bdd49ead41a8c8f7547c069
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-02-10 19:04:37 -0500

    update active record 5.2  & run migrations

--- db/migrate/20210210223046_create_collections.rb
+++ db/migrate/20210210223046_create_collections.rb
@@ -1,10 +1,11 @@
-class CreateCollections < ActiveRecord::Migration[6.0]
+class CreateCollections < ActiveRecord::Migration[5.2]
   def change
     create_table :collections do |t|
       t.string :destination
       t.string :start_date
       t.string :end_date
       t.text :trip_summary 
+      t.references :user, foreign_key: {on_delete: :cascade} 
     end 
   end
 end
