7e7747488371c106d4f909bede5e1dbdc8f04c83
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-02-11 18:12:41 -0500

    configure app_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < Sinatra::Base
+  require 'securerandom'
+  
+  configure do
+    set :views, 'app/views'
+    set :public_folder, 'public'
+    enable :sessions
+    set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
+  end
 
   get '/' do 
     erb :index
   end 
-  
+
 end
