88379275efa2014c5ed8ac27c8a2d25b9309f736
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-02-22 09:26:36 -0500

    define user slug and find_by_slug methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
     has_many :collections 
+    has_secure_password 
+
+    def slug 
+        username.donwcase.gsub("","-")
+    end 
     
+    def self.find_by_slug(slug)
+        User.all.find{|user| user.slug == slug}
+    end 
 end
