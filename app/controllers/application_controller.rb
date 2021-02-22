726aa67d584f40d12fdbcc20034142eabe8faa70
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-02-22 09:16:11 -0500

    add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,32 @@
+#require 'securerandom'
 class ApplicationController < Sinatra::Base
-  require 'securerandom'
-  
+
   configure do
     set :views, 'app/views'
     set :public_folder, 'public'
     enable :sessions
-    set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) } #fix this later 
+    set :session_secret, "myvaca_so_secret"
   end
 
   get '/' do 
     erb :index
   end 
+
+  helpers do 
+    def logged_in? 
+      !!current_user 
+    end 
+
+    def current_user 
+      @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end 
+
+    def redirect_to_login 
+      redirect "/users/login"
+    end 
+
+    def redirect_to_homepage
+      redirect "/users/:slug"
+    end 
 
 end
