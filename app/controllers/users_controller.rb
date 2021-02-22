64f772e84f83bd61844d18e4bac8a3a3d0d9f7be
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-02-22 11:17:46 -0500

    build get /signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,26 @@
 class UsersController < ApplicationController
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/homepage'
+  end
+
+  get '/signup' do 
+    if !logged_in?
+    erb :'users/signup' , locals: {message: "Please finish signing up before you sign in"}
+    else 
+      redirect_to_homepage
+    end 
+  end 
+
+  post '/signup' do 
+    erb :'users/homepage'
+  end 
 
   get '/login' do
     erb :'/users/login'
   end
 
   post '/login' do 
-    erb :'users/homepage'
-  end 
-
-  get '/users/:slug' do
-    @user = User.find_by(email: params["email"], password: params["password"])
-    session[:id] = @user.id  
-    erb :'users/homepage'
-  end
-
-  get '/signup' do 
-    erb :'users/signup'
-  end 
-
-  post '/signup' do 
     erb :'users/homepage'
   end 
 
