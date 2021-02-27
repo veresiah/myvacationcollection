24a86fba88ef0fe93aac2de805307ee51fa61814
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-02-27 09:23:09 -0500

    build 'get' collections routes

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,2 +1,20 @@
 class CollectionsController < ApplicationController
+    get '/collections' do 
+        @collections = Collection.all 
+        erb :'collections/index'
+    end 
+
+    get 'collections/new' do    
+        erb :'collections/new'
+    end  
+
+    get 'collections/:id' do 
+        @collection = Collection.find(params[:id])
+        erb :'collections/show'
+    end 
+
+    get 'collections/:id/edit' do 
+    @collection = Collection.find(params[:id])
+        erb :'collections/edit'
+    end 
 end 
