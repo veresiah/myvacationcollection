2f6133f62906cc1f5bb6db63b0079f19bf8d7a2f
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-03-05 17:48:45 -0500

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
     has_many :collections 
     has_secure_password 
+    validates :username, :email, presence: true, uniqueness: true
+    validates :first_name, :last_name, presence: true
 
     def slug 
         username.downcase.gsub(" ","-")
