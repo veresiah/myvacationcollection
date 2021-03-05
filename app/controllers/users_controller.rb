e58f18dc59f6dfbfa9789f4634d7dde3aff615f6
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-03-05 18:17:54 -0500

    change user -> @user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   post '/login' do 
     @user = User.find_by(:username => params[:username])
     if @user && @user.authenticate(params[:password])
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       flash[:message] = "Logged in Successfully"
       redirect_to_homepage
     else 
